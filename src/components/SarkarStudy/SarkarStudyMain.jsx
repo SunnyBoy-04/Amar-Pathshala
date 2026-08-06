9017c382bf6c7ec705f121da037d31420d34d410
Rakeshpatra181 <rakeshpatra.dhj@gmail.com>
2026-08-06 19:02:00 +0530

    Fix missing useEffect import in SarkarStudyMain.jsx

--- src/components/SarkarStudy/SarkarStudyMain.jsx
+++ src/components/SarkarStudy/SarkarStudyMain.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ExamDetailHub from '../ExamPage/ExamDetailHub';
 import TypewriterSearchBar from './TypewriterSearchBar';
 import FloatingBotWidget from '../Bot/FloatingBotWidget';
